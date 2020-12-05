04a92f8d5f2f0f39dcf6937fc478b725090d1fed
unknown <johpetsc@gmail.com>
2020-12-05 02:11:00 -0300

    Comments

--- src/detection.py
+++ src/detection.py
@@ -4,68 +4,75 @@ from PIL import Image
 import glob as glob
 import recognition as rec
 
-PATH_TO_MODEL = '../models/detection/frozen_inference_graph.pb'
-PATH_TO_TEST_IMAGES = glob.glob('../test_images/*.jpg')
+PATH_TO_MODEL = 'models/detection/frozen_inference_graph.pb' #Path to the pre-trained model
+PATH_TO_TEST_IMAGES = glob.glob('test_images/*.jpg') #Path to test images
 
+#Gets an image, detection graph and session, returns a list of detections and scores
 def detection(image_np, detection_graph, sess):
     image_np_expanded = np.expand_dims(image_np, axis=0)
+    #Gets the detection graph tensors
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
     boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
     scores = detection_graph.get_tensor_by_name('detection_scores:0')
     classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
     
+    #Executes the detection
     (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections],
                                                         feed_dict={image_tensor: image_np_expanded})
 
     return boxes, scores
 
+#Filters detections with less than 20% probability
 def filter_detections(detected, boxes, scores, w, h):
-    count = 0
-    for box, score in zip(np.squeeze(boxes), np.squeeze(scores)):
-        if score*100 > 20: 
-            box[0], box[1], box[2], box[3] = box[0]*h, box[1]*w, box[2]*h, box[3]*w
-            detected.append(box)
+    count = 0 #Amount of valid detections
+    for box, score in zip(np.squeeze(boxes), np.squeeze(scores)): #Bounding box and score from each detection
+        if score*100 > 20: #>20%
+            box[0], box[1], box[2], box[3] = box[0]*h, box[1]*w, box[2]*h, box[3]*w #Normalized bounding boxes coordinates
+            detected.append(box) #Append to list with filtered detections
             count += 1
     return detected, count
 
+#Filters detections with less than 20% probability in the rotated image
 def filter_detections_rotated(detected, boxes, scores, w, h):
-    for box, score in zip(np.squeeze(boxes), np.squeeze(scores)):
-        if score*100 > 20: 
-            box[0], box[1], box[2], box[3] = h-(box[2]*h), w-(box[3]*w), h-(box[0]*h), w-(box[1]*w)
-            detected.append(box)
+    for box, score in zip(np.squeeze(boxes), np.squeeze(scores)): #Bounding box and score from each detection
+        if score*100 > 20: #>20%
+            box[0], box[1], box[2], box[3] = h-(box[2]*h), w-(box[3]*w), h-(box[0]*h), w-(box[1]*w) #Normalized bounding boxes coordinates and transform to the real coordinates
+            detected.append(box) #Append to list with filtered detections
     return detected
 
 def main():
     detection_graph = tf.Graph()
-    learn_inf = rec.getModel()
+    learn_inf = rec.getModel() #Loads the recognition model
 
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
 
-    with detection_graph.as_default():
+    with detection_graph.as_default(): #Loads the detection model (frozen graph)
         od_graph_def = tf.compat.v1.GraphDef()
         fid = tf.io.gfile.GFile(PATH_TO_MODEL, 'rb')
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
-        sess = tf.compat.v1.Session(graph=detection_graph, config=config)
-
-        for image_path in PATH_TO_TEST_IMAGES:
+        sess = tf.compat.v1.Session(graph=detection_graph, config=config) #Initializes the session
+        for image_path in PATH_TO_TEST_IMAGES: #For each image in the test images folder
             image = Image.open(image_path)
             w, h = image.size
             detected = []
 
-            image_np = np.array(image.getdata()).reshape((h, w, 3)).astype(np.uint8)
-            image_np_r = np.array(image.rotate(180).getdata()).reshape((h, w, 3)).astype(np.uint8)
+            image_np = np.array(image.getdata()).reshape((h, w, 3)).astype(np.uint8) #Image to numpy array
+            image_np_r = np.array(image.rotate(180).getdata()).reshape((h, w, 3)).astype(np.uint8) #Rotated image to numpy array
 
+            #Detect and filter detections for the normal image
             boxes, scores = detection(image_np, detection_graph, sess)
             detected, num_detections = filter_detections(detected, boxes, scores, w, h)
             
+            #Detect and filter detections for the rotated image
             boxes, scores = detection(image_np_r, detection_graph, sess)
             detected = filter_detections_rotated(detected, boxes, scores, w, h)
-                    
+
+            #Predicts which traffic sign each detection is      
             rec.predict(image_np, learn_inf, detected, num_detections)
 
 if __name__ == '__main__':
--- src/recognition.py
+++ src/recognition.py
@@ -3,59 +3,68 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from fastai.data.all import *
 from fastai.vision.all import *
+from fastai.callback.all import *
 import torch
 import cv2 as cv
 
+#Function fo training
 def train():
     signs = DataBlock(
-        blocks=(ImageBlock, CategoryBlock), 
+        blocks=(ImageBlock, CategoryBlock),
         get_items=get_image_files, 
-        splitter=RandomSplitter(valid_pct=0.2, seed=42),
-        get_y=parent_label,
-        item_tfms=Resize(128))
+        splitter=RandomSplitter(valid_pct=0.2, seed=42), #20% of the images for validation
+        get_y=parent_label, #Folder name as label
 
-    dls = signs.dataloaders('../dataset/Training/GTSRB/Final_Training/Images', num_workers=0, bs=16)
-    dls.valid.show_batch(max_n=15, nrows=3)
+    dls = signs.dataloaders('dataset/Training/GTSRB/Final_Training/Images', num_workers=0, bs=16) #Load the Training dataset with batch size of 16 (higher means faster but less accurate)
+    dls.valid.show_batch(max_n=15, nrows=3)#Show batch
+    plt.show()#Not needed if running on Jupyter or Collab
+
+    #Train the model
+    learn = cnn_learner(dls, resnet50, metrics=[accuracy, error_rate])#Learning parameters, resnet50 got the best results, but resnet34 is also good. No improvement noted with resnet101+
+    learn.model.cuda()#Use this if fastai is using your CPU instead of the GPU
+    learn.fine_tune(5)#Number of iterations, more than 6 is unnecessary
+    learn.recorder.plot_loss()#Shows valid and train loss
     plt.show()
 
-    learn = cnn_learner(dls, resnet34, metrics=error_rate)
-    learn.model.cuda()
-    learn.fine_tune(5)
-
+    #Show confusion matrix
     interp = ClassificationInterpretation.from_learner(learn)
-    interp.plot_confusion_matrix()
+    interp.plot_confusion_matrix(figsize=(13,13))
     plt.show()
 
-    interp.plot_top_losses(15, nrows=3)
+    #Show top losses
+    interp.plot_top_losses(20, nrows=5)
     plt.show()
 
-    learn.export('../models/recognition/export.pkl')
+    #Export the trained model
+    learn.export('models/recognition/export.pkl')
 
+#Function for predictions.
+#Parameters: image, the recognition model and a list of detection boxes.
 def predict(img, learn_inf, boxes, num_detections):
-    image = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-    count = 0
-    for box in boxes:
-        yi, yf, xi, xf = int(box[0]), int(box[2]), int(box[1]), int(box[3])
-        sign = img[yi:yf, xi:xf]
-        pred, pred_idx, probs = learn_inf.predict(sign)
-        output = pred + '[' + str(round(float(probs[pred_idx]*100))) + '%]'
-        if count < num_detections or pred == 'yield':
-            print('Prediction: ', pred, 'Probability: %5.2f' % (probs[pred_idx]*100), '%')
-            cv.rectangle(image, (xi, yi), (xf, yf), (0, 0, 0), 5)
+    image = cv.cvtColor(img, cv.COLOR_RGB2BGR)#Transforming image from RGB to BGR because of OpenCV
+    for box in boxes: #Each detection box in the list
+        yi, yf, xi, xf = int(box[0]), int(box[2]), int(box[1]), int(box[3])#bounding box coordinates in the image
+        sign = img[yi:yf, xi:xf]#bounding box content to variable
+        pred, pred_idx, probs = learn_inf.predict(sign)#prediction
+        output = pred + '[' + str(round(float(probs[pred_idx]*100))) + '%]'#Prediction label and probability from tensor to rounded float to string
+        if num_detections > 0 or pred == 'yield':#Gets the yield sign from the rotated image detection
+            print('Prediction: ', output)
+            cv.rectangle(image, (xi, yi), (xf, yf), (0, 0, 0), 5)#Draws the bounding box, white rectangle with black borders
             cv.rectangle(image, (xi, yi), (xf, yf), (255, 255, 255), 3)
-            cv.putText(image, output, (xi-40, yi), 2, 1, (0, 0, 0), lineType=cv.LINE_AA, thickness=2)
+            cv.putText(image, output, (xi-40, yi), 2, 1, (0, 0, 0), lineType=cv.LINE_AA, thickness=2)#Draws the prediction, white text with black borders
             cv.putText(image, output, (xi-40, yi), 2, 1, (255, 255, 255), lineType=cv.LINE_AA, thickness=1)
-            count += 1
+            num_detections -= 1
     
-    cv.imshow("img", image)
-    cv.waitKey(0)
+    cv.imshow("img", image)#Shows image
+    if cv.waitKey(0) & 0xFF == ord('q'):#Press Q to exit
+        exit(0)
 
+#Function used to load the already trained model
 def getModel():
-    learn_inf = load_learner('export.pkl')
+    learn_inf = load_learner('models/recognition/export.pkl')
 
     return learn_inf
 
 if __name__ == '__main__':
+    #Train the model
     train()
-    #learn_inf = getModel()
-    #predict('../dataset/Test/00000.jpg', learn_inf)
